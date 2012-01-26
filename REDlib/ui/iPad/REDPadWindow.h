c5b03bed218006c667d39eb379061dfaeea3385b
Tom Seago <tom@tomseago.com>
2012-01-26 01:38:55 -0800

    Some iPad files

---
+++ REDlib/ui/iPad/REDPadWindow.h
@@ -0,0 +1,13 @@
+//
+//  REDPadWindow.h
+//  REDlib
+//
+//  Created by Tom Seago on 12/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "REDWindow.h"
+
+@interface REDPadWindow : REDWindow
+
+@end
---
+++ REDlib/ui/iPad/REDPadWindow.m
@@ -0,0 +1,23 @@
+//
+//  REDPadWindow.m
+//  REDlib
+//
+//  Created by Tom Seago on 12/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "REDPadWindow.h"
+
+@implementation REDPadWindow
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
